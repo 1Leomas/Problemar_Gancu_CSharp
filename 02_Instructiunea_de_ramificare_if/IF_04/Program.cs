7a0517588435b45aa4e57f71a831852372531ae7
Sam Rutcovschi <samuil.rutcovschi@gmail.com>
2022-06-10 22:44:05 +0300

    add IF_04

--- 02_Instructiunea_de_ramificare_if/IF_04/Program.cs
+++ 02_Instructiunea_de_ramificare_if/IF_04/Program.cs
@@ -6,7 +6,18 @@ namespace IF_04
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Introduceti notele lui Ionel: ");
+            Console.Write("Nota1: ");
+            int nota1 = int.Parse(Console.ReadLine());
+            Console.Write("Nota2: ");
+            int nota2 = int.Parse(Console.ReadLine());
+            Console.Write("Nota3: ");
+            int nota3 = int.Parse(Console.ReadLine());
+
+            Console.Write("Notele pe care le-a zis parintilor sunt:");
+            if (nota1 >= 7) Console.Write($" {nota1}");
+            if (nota2 >= 7) Console.Write($" {nota2}");
+            if (nota3 >= 7) Console.Write($" {nota3}");
         }
     }
 }
